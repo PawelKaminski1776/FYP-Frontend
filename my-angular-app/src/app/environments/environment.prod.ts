5ee68408d485abd8d159e01ced6ee6aaf6c86515
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-03-30 21:13:03 +0100

    Changed environment urls to proper ones for cloud

--- my-angular-app/src/app/environments/environment.prod.ts
+++ my-angular-app/src/app/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
-    UserDetailsapiUrl: 'http://localhost:5001',
-    CheckLoginapiUrl: 'http://localhost:5003',
-    SendTrainingDataapiUrl: 'http://localhost:5005',
-    ReceiveModelDataApiUrl: 'http://localhost:5009',
-    GetImagesAndAnnotationsApiUrl: 'http://localhost:5011',
-    SendImagesAndAnnotationsApiUrl: 'https://localhost:5014',
+    UserDetailsapiUrl: 'https://userdetails-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
+    CheckLoginapiUrl: 'https://checklogin-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
+    SendTrainingDataapiUrl: 'https://sendtrainingdata-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
+    ReceiveModelDataApiUrl: 'https://receivemodeldata-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
+    GetImagesAndAnnotationsApiUrl: 'https://getimagesandannotations-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
+    SendImagesAndAnnotationsApiUrl: 'https://sendimagesandannotations-dot-project-fyp-u2f3hue32ui32.nw.r.appspot.com',
     featureFlag: true
   };
