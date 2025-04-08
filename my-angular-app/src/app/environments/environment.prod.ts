48f0792fb45124ac98652ad407f1686c5715f888
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-04-08 17:06:33 +0100

    Changed environment files

--- my-angular-app/src/app/environments/environment.prod.ts
+++ my-angular-app/src/app/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: true,
-  UserDetailsapiUrl: 'http://10.154.0.28:5001',
-  CheckLoginapiUrl: 'http://10.154.0.28:5003',
-  SendTrainingDataapiUrl: 'http://10.154.0.28:5005',
-  ReceiveModelDataApiUrl: 'http://10.154.0.28:5009',
-  GetImagesAndAnnotationsApiUrl: 'http://10.154.0.28:5011',
-  SendImagesAndAnnotationsApiUrl: 'https://10.154.0.28:5014',
+  production: false,
+  UserDetailsapiUrl: 'http://localhost:5001',
+  CheckLoginapiUrl: 'http://localhost:5003',
+  SendTrainingDataapiUrl: 'http://localhost:5005',
+  ReceiveModelDataApiUrl: 'http://localhost:5009',
+  GetImagesAndAnnotationsApiUrl: 'http://localhost:5011',
+  SendImagesAndAnnotationsApiUrl: 'https://localhost:5014',
   featureFlag: true
 };
