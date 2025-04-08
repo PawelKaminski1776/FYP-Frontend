286952cd3bba60c8adc202bb9a5455aed0f2af04
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-04-08 17:29:46 +0100

    Ip fixes

--- my-angular-app/src/app/environments/environment.prod.ts
+++ my-angular-app/src/app/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  UserDetailsapiUrl: 'http://localhost:5001',
-  CheckLoginapiUrl: 'http://localhost:5003',
-  SendTrainingDataapiUrl: 'http://localhost:5005',
-  ReceiveModelDataApiUrl: 'http://localhost:5009',
-  GetImagesAndAnnotationsApiUrl: 'http://localhost:5011',
-  SendImagesAndAnnotationsApiUrl: 'https://localhost:5014',
+  UserDetailsapiUrl: 'http://34.147.239.239:5001',
+  CheckLoginapiUrl: 'http://34.147.239.239:5003',
+  SendTrainingDataapiUrl: 'http://34.147.239.239:5005',
+  ReceiveModelDataApiUrl: 'http://34.147.239.239:5009',
+  GetImagesAndAnnotationsApiUrl: 'http://34.147.239.239:5011',
+  SendImagesAndAnnotationsApiUrl: 'https://34.147.239.239:5014',
   featureFlag: true
 };
