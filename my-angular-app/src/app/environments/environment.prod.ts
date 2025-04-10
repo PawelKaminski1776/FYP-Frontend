2df413a93ae57108779b899195412bf52e319a19
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-04-10 23:28:08 +0100

    Fixed ip

--- my-angular-app/src/app/environments/environment.prod.ts
+++ my-angular-app/src/app/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: false,
-  UserDetailsapiUrl: 'http://34.147.239.239:5001',
-  CheckLoginapiUrl: 'http://34.147.239.239:5003',
-  SendTrainingDataapiUrl: 'http://34.147.239.239:5005',
-  ReceiveModelDataApiUrl: 'http://34.147.239.239:5009',
-  GetImagesAndAnnotationsApiUrl: 'http://34.147.239.239:5011',
-  SendImagesAndAnnotationsApiUrl: 'https://34.147.239.239:5014',
+  production: true,
+  UserDetailsapiUrl: 'http://35.189.120.1:5001',
+  CheckLoginapiUrl: 'http://35.189.120.1:5003',
+  SendTrainingDataapiUrl: 'http://35.189.120.1:5005',
+  ReceiveModelDataApiUrl: 'http://35.189.120.1:5009',
+  GetImagesAndAnnotationsApiUrl: 'http://35.189.120.1:5011',
+  SendImagesAndAnnotationsApiUrl: 'https://35.189.120.1:5014',
   featureFlag: true
 };
