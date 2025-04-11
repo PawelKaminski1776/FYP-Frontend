d7e3bba25436b9d2d16f564bf7da6bbd325983f7
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-04-11 09:18:44 +0100

    Fixed jenkinsfile

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './Homepage/Homepage.component'; 
-import { LoadingComponent } from './loading/loading.component';
 import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app.routes';
 import { LoginpageComponent } from './Loginpage/Loginpage.component';
-import { InspectiontrainingpageComponent } from './Inspectionscrapingpage/Inspectionscrapingpage.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -14,9 +12,7 @@ import { CommonModule } from '@angular/common';
   declarations: [
     AppRoutingModule,
     AppComponent,
-    LoadingComponent,
     HomepageComponent,
-    InspectiontrainingpageComponent,
     LoginpageComponent,
   ],
   imports: [
