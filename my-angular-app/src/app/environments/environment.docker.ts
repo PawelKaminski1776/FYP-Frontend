cd3c8d47dccc8891e287dd24edce4251d176090b
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-04-11 18:58:42 +0100

    changed url

--- my-angular-app/src/app/environments/environment.docker.ts
+++ my-angular-app/src/app/environments/environment.docker.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  UserDetailsapiUrl: 'http://35.189.120.1:5001',
-  CheckLoginapiUrl: 'http://35.189.120.1:5003',
-  SendTrainingDataapiUrl: 'http://35.189.120.1:5005',
-  ReceiveModelDataApiUrl: 'http://35.189.120.1:5009',
-  GetImagesAndAnnotationsApiUrl: 'http://35.189.120.1:5011',
-  SendImagesAndAnnotationsApiUrl: 'https://35.189.120.1:5014',
+  UserDetailsapiUrl: 'http://34.147.254.222:5001',
+  CheckLoginapiUrl: 'http://34.147.254.222:5003',
+  SendTrainingDataapiUrl: 'http://34.147.254.222:5005',
+  ReceiveModelDataApiUrl: 'http://34.147.254.222:5009',
+  GetImagesAndAnnotationsApiUrl: 'http://34.147.254.222:5011',
+  SendImagesAndAnnotationsApiUrl: 'https://34.147.254.222:5014',
   featureFlag: true
 };
